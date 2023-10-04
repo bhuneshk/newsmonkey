d4a4923edb345c5905726b511cacd56a4cff5290
bhuneshk <kumarbhunesh3@gmail.com>
2023-10-05 00:07:15 +0530

    next and prev updated

--- src/components/News.js
+++ src/components/News.js
@@ -2,30 +2,51 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    state={
-        articles: [],
-        loading: false
-    }
+
 
     constructor(){
     super();
-    // this.State={
-    //     articles: [],
-    //     loading: false
-    // }
+    this.state={
+        articles: [],
+        loading: false,
+        page: 1,
+        totalLenght: 0
+    }
   }
 
   async componentDidMount() {
-    let url= 'https://newsapi.org/v2/top-headlines?country=us&apiKey=7b059855142940549b04611f0728f5d6';
+    let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=7b059855142940549b04611f0728f5d6&pageSize=10`;
     let data = await fetch(url);
     let parsedData= await data.json();
-    console.log(parsedData);
     this.setState({
-        articles : parsedData.articles
+        articles : parsedData.articles,
+        totalLenght: parsedData.articles.length
+    })
+}
+  handletoPrev=async ()=>{
+    let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=7b059855142940549b04611f0728f5d6&page=${this.state.page-1}&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData= await data.json();
+    this.setState({
+        articles : parsedData.articles,
+        page: this.state.page-1,
+        totalLenght: parsedData.articles.length
+    })
+  }
+
+  handletoNext=async ()=>{
+    let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=7b059855142940549b04611f0728f5d6&page=${this.state.page+1}&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData= await data.json();
+    this.setState({
+        articles : parsedData.articles,
+        page: this.state.page+1,
+        totalLenght: parsedData.articles.length
 
     })
-    console.log(this.articles);
-}
+    
+  }
+
   render() {
     return (
         <div className='container my-3'>
@@ -36,6 +57,10 @@ export class News extends Component {
                     <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
                  </div>
                 })}
+              </div>
+              <div className="container d-flex justify-content-between">
+              <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handletoPrev} >&larr; Previous</button>
+              <button disabled={this.state.totalLenght<10} type="button" className="btn btn-dark" onClick={this.handletoNext} >Next &rarr;</button>
               </div>
         </div>
     )
